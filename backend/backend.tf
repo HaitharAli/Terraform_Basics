04bd6cecdc5a53f827f51dcef4896ff53acf61ed
HaitharAli <112061941+HaitharAli@users.noreply.github.com>
2022-11-14 12:59:23 +0530

    removing keys

--- backend/backend.tf
+++ backend/backend.tf
@@ -3,7 +3,7 @@ terraform {
     key = "terraform/tfstate.tfstate"
     bucket = "remote-backend-haithar"
     region = "us-east-1"
-    access_key = "AKIAZUCQ5R7HUEIRKEFD"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
   }
 }
